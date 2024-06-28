879292a5cdda1d78f2b30d402b04be056d6c32a6
Priyank-Aggarwal1024 <priyankaggarwal6395@gmail.com>
2024-06-28 19:11:38 +0530

    feat: change routing

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Layout />} />
-          <Route path='/packpayment' element={<PaymentPage />} />
+          <Route path='/home' element={<Layout />} />
+          <Route path='/' element={<PaymentPage />} />
         </Routes>
       </BrowserRouter>
     </>
